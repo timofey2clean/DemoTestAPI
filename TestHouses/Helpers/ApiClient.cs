2647c808ee4ec94a6e81aeee5c151512678c0fa9
timofey2clean <timofey@yandex.ru>
2024-11-25 17:16:55 +0300

    updated

---
+++ TestHouses/Helpers/ApiClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TestHouses.Helpers
+{
+    class ApiClient
+    {
+        public static T GetObject<T>(string url)
+        {
+            const string method = "GET";
+
+            string responseBody = SendWebRequest(url, method);
+
+            if (!IsValidJson<T>(responseBody))
+            {
+                throw new Exception("Received Json is invalid.");
+            }
+
+            return JsonConvert.DeserializeObject<T>(responseBody);
+        }
+
+        private static string SendWebRequest(string url, string method)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception(string.Format("Web request failed. Response code: {0}", (int)response.StatusCode));
+            }
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        private static bool IsValidJson<T>(string jsonBody)
+        {
+            jsonBody = jsonBody.Trim();
+
+            try
+            {
+                if (jsonBody.StartsWith("{") && jsonBody.EndsWith("}"))
+                {
+                    JToken.Parse(jsonBody);
+                }
+                else if (jsonBody.StartsWith("[") && jsonBody.EndsWith("]"))
+                {
+                    JArray.Parse(jsonBody);
+                }
+                else
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- TestHouses/HousesApiTest.cs
+++ TestHouses/HousesApiTest.cs
@@ -1,45 +1,63 @@
-﻿using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TestHouses.Helpers;
+using TestHouses.Models;
 
 namespace TestHouses
 {
-    [TestClass]
+    [TestFixture]
     public class HousesApiTest
     {
-        const string BaseUrl = "https://www.anapioficeandfire.com/api/houses";
+        private const string BaseUrl = "https://www.anapioficeandfire.com/api/houses";
 
-        [TestCategory("FilterTest")]
-        [DataRow("Dorne" , true)]
-        [DataTestMethod]
+        [Test]
+        [Description("Check test houses response filtered by region and haswords")]
+        [TestCase("Dorne", true)]
         public void TestRegionAndHaswordsFilter(string region, bool haswords)
         {
-            // Get all houses
-            ApiObjects.House[] allHouses = Helpers.ApiRequester.GetJsonObject<ApiObjects.House[]>(BaseUrl);
-            Assert.IsNotNull(allHouses);
-            Assert.IsTrue(allHouses.Any(), "Received empty house array.");
+            // arrange
+            var filterUrl = $"{BaseUrl}?region={region}&haswords={haswords.ToString().ToLower()}";
 
-            // Get houses using filters
-            string filterUrl = string.Format("{0}?region={1}&haswords={2}", BaseUrl, region, haswords.ToString().ToLower());
-            ApiObjects.House[] filteredHouses = Helpers.ApiRequester.GetJsonObject<ApiObjects.House[]>(filterUrl);
-            Assert.IsNotNull(filteredHouses);
+            // act
+            var allHouses = ApiClient.GetObject<House[]>(BaseUrl);
+            allHouses.Should().NotBeNullOrEmpty();
+       
+            var filteredHouses = ApiClient.GetObject<House[]>(filterUrl);
+            filteredHouses.Should().NotBeNullOrEmpty();
 
-            // Check all filtered houses have expected region and haswords
-            Assert.IsTrue(filteredHouses.All(_ => string.Equals(_.region, region)), "Some houses received using filter have unexpected region.");
-            Assert.IsTrue(filteredHouses.All(_ => haswords == !string.IsNullOrWhiteSpace(_.words)), "Some houses received using filter have unexpected words property.");
+            // assert
+            // Check all filtered houses region
+            filteredHouses.Should().AllSatisfy(
+                _ => string.Equals(_.region, region),
+                "Some houses received using filter have unexpected region.");
+
+            // Check all filtered houses words
+            filteredHouses.Should().AllSatisfy(
+                _ => haswords.Equals(!string.IsNullOrEmpty(_.words)),
+                "Some houses received using filter have unexpected words property.");
 
             // Check number of houses received using filter is equal to number of such houses in all houses
-            ApiObjects.House[] selectedFromAllHouses = allHouses.Where(_ => string.Equals(_.region, region) && haswords == !string.IsNullOrWhiteSpace(_.words)).ToArray();
-            Assert.IsTrue(selectedFromAllHouses.Count() == filteredHouses.Count(), "Initial collection of houses contains less houses with parameters given in the filter.");
+            allHouses.Where(
+                _ => string.Equals(_.region, region)
+                && haswords == !string.IsNullOrWhiteSpace(_.words))
+                .Count()
+                .Should().Be(filteredHouses.Count(),
+                "Initial collection of houses contains less houses with parameters given in the filter.");
         }
 
-        [TestCategory("NegativeTest")]
-        [DataRow("http://notexistingurl.notexistingurl.notexistingurl")]
-        [ExpectedException(typeof(System.Net.WebException), "Test method should throw System.Net.WebException.")]
-        [DataTestMethod]
-        public void TestInvalidUrl(string url)
+        [Test]
+        [Description("NegativeTest - URL does not exist")]
+        public void TestInvalidUrl()
         {
-            // This method should throw WebException
-            Helpers.ApiRequester.GetJsonObject<ApiObjects.House[]>(url);
+            const string NotExistingUrl = "http://notexistingurl.notexistingurl.notexistingurl";
+
+            // arrange
+            Action getHousesAction = () => ApiClient.GetObject<House[]>(NotExistingUrl);
+
+            // act & assert
+            getHousesAction.Should().Throw<System.Net.WebException>("Test method should throw System.Net.WebException.");
         }
     }
 }
